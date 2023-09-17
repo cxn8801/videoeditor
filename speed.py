a5d91ae9d9cfe024983aff98a7a1edb3440be46a
cxn8801 <326248080@qq.com>
2023-09-17 17:18:34 +0800

    视频加速

---
+++ speed.py
@@ -0,0 +1,15 @@
+# Import everything needed to edit video clips
+from moviepy.editor import *
+
+# loading video gfg
+clip = VideoFileClip("input.mp4")
+
+
+# getting subclip from it
+clip = clip.subclip(0, 9)
+
+# applying speed effect
+final = clip.fx( vfx.speedx, 5)
+
+# showing final clip
+final.write_videofile('output.mp4')

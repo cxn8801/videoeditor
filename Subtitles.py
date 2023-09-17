207a8b625804586ef26a9245bf58e69648ffb1cb
cxn8801 <326248080@qq.com>
2023-09-17 16:52:12 +0800

    中文字幕

--- Subtitles.py
+++ Subtitles.py
@@ -1,15 +1,20 @@
 from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-generator = lambda txt: TextClip(txt, font='Arial', fontsize=24, color='white')
-subs = [((0, 4), 'subs1'),
-        ((4, 9), 'subs2'),
+
+print ( TextClip.list("font") )
+
+generator = lambda txt: TextClip(txt, font='SimHei', fontsize=24, color='white')
+subs = [((0, 4), '你好字幕'),
+        ((4, 9), '你好字幕'),
         ((9, 12), 'subs3'),
         ((12, 16), 'subs4')]
 
 subtitles = SubtitlesClip(subs, generator)
+# subtitles = SubtitlesClip("demo.srt", generator)
 
 video = VideoFileClip("chaplin-modern-times-non-sense-song-720.mp4")
 result = CompositeVideoClip([video, subtitles.set_pos(('center','bottom'))])
 
 result.write_videofile("output.mp4", fps=video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
+
